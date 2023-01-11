3981ee487a015805bf80f1b6905c3518fff0c40b
liu <a2506991786@qq.com>
2023-01-11 22:47:35 +0800

    topview

--- src/basic/Pokemon/KnifeJANG.java
+++ src/basic/Pokemon/KnifeJANG.java
@@ -4,7 +4,7 @@ public class KnifeJANG extends Pokemon{
     public KnifeJANG() {
         super("刀酱");
         this.HP=6666;
-        this.attack=66;
-        this.defense=66;
+        this.attack=45;
+        this.defense=80;
     }
 }
